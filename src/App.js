4345b0da5a013931a940ea4ac1211391c93ea47b
salih <salihkilinc80@gmail.com>
2023-01-09 19:17:03 +0100

    v1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+//import Products from "./componenet/container/product";
+
+import Button from "./componenet/button/Button";
+import Form from "./componenet/form/Form";
+import Input from "./componenet/input/Input";
+import Title from "./componenet/sectiontitle/title";
+
+
+
+
+
+function App() {
+  return (<div>
+    {/*<Products/>*/}
+   <>
+   </>
+    <Title/>
+    <Form>
+    
+      <Input />
+      <Input />
+      <Button />
+    </Form>
+
+
+  </div>
+  )
+}
+
+export default App;
